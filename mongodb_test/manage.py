b839637de73c3e6c64db0f22b614093a9b033fb4
Alex Yang <yanghs.dgut@gmail.com>
2016-11-15 23:26:46 +0800

    Add find_user

--- mongodb_test/manage.py
+++ mongodb_test/manage.py
@@ -1,5 +1,5 @@
-# 1.未使用ORM
 # # encodeing = utf-8
+# 1.未使用ORM
 
 # from flask_script import Manager
 # from app import app
@@ -41,7 +41,7 @@ manager = Manager(app)
 def save():
     # user = User('jike', 'helloworld@qq.com')
     # user.save()
-    user = User(name = 'hello2', email = 'hello2@qq.com')
+    user = User(name = 'hello3', email = 'hello3@qq.com')
     user.save()
 
 @manager.command
@@ -50,6 +50,17 @@ def query_users():
     users = User.objects.all()
     for user in users:
         print (user)
+
+@manager.command
+def find_user(var):
+    # user = User.objects(__raw__ = {'name':<var>})
+    # user = User.objects(name='hello3', email = 'hello3@qq.com')
+    user = User.objects(name=var)
+    print (user)
+    # users = User.objects.all()
+    # for user in users:
+    #     if user.name == name:
+    #         print (user)
 
 if __name__  == '__main__':
     manager.run()
