3d0943e7ead2d648cea530473dd66805c81f2fd8
Alex Yang <yanghs.dgut@gmail.com>
2016-12-27 21:56:24 +0800

    Add post Method for jsonrestful.py

--- restful/jsonrestful.py
+++ restful/jsonrestful.py
@@ -1,13 +1,14 @@
 # encoding utf-8 
 
 from flask import  Flask, request , abort
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask import send_from_directory
 import os
 
 
 app = Flask(__name__)
 api = Api(app)
+
 
 class Hotnews(Resource):
     def get(self):
@@ -16,14 +17,26 @@ class Hotnews(Resource):
 api.add_resource(Hotnews, '/hotnews')
 
 
+parser = reqparse.RequestParser()
+parser.add_argument('imageid')
+
 class Download(Resource):
     def get(self, imageid):
-       if request.method == 'GET':
-           if os.path.isfile(os.path.join('images/',imageid)):
-               return send_from_directory('images', imageid, as_attachment =True)
-           abort(404)
+       # if request.method == 'GET':
+        if os.path.isfile(os.path.join('images/',imageid)):
+            return send_from_directory('images', imageid, as_attachment =True)
+        abort(404)
 
-api.add_resource(Download, '/images/<string:imageid>')
+    def post(self):
+        args = parser.parse_args()
+        imageid = args['imageid']
+        if os.path.isfile(os.path.join('images/', imageid)):
+            return send_from_directory('images', imageid, as_attachment=True)
+        abort(404)
+
+api.add_resource(Download, '/images/<string:imageid>', '/images')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
